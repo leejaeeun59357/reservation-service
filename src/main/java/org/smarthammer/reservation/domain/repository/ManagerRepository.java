04646b081a4c9d95c15d4e93c145e23f07a47e2e
jaeeun <ne2227@naver.com>
2024-03-06 20:58:14 +0900

    feat : [002-Manager-SignUp] Manager 의 로그인 구현

--- src/main/java/org/smarthammer/reservation/domain/repository/ManagerRepository.java
+++ src/main/java/org/smarthammer/reservation/domain/repository/ManagerRepository.java
@@ -1,13 +1,12 @@
 package org.smarthammer.reservation.domain.repository;
 
-import org.smarthammer.reservation.domain.model.Consumer;
+import org.smarthammer.reservation.domain.model.Manager;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface ConsumerRepository extends JpaRepository<Consumer, Long> {
-
-    Optional<Consumer> findByEmail(String email);
+public interface ManagerRepository extends JpaRepository<Manager, Long> {
+    Optional<Manager> findByEmail(String email);
 }
