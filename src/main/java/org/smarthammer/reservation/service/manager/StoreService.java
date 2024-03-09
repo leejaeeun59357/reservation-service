cda29c6de21ca32cfca120a5801cf063b7a716ad
Leejaeeun <ne2227@naver.com>
2024-03-09 12:32:02 +0900

    feat : [006-Store-Updae] 등록된 상점의 이름과 설명 변경 구현

--- src/main/java/org/smarthammer/reservation/service/manager/StoreService.java
+++ src/main/java/org/smarthammer/reservation/service/manager/StoreService.java
@@ -32,7 +32,7 @@ public class StoreService {
     /**
      * form 을 통해 입력받은 managerId에 따른 manager 객체가 존재하는지 검사
      * @param form
-     * @return ManagerDto
+     * @return Manager
      */
     public Optional<Manager> findManager(AddStoreForm form) {
         Optional<Manager> findManagerValue = managerRepository.findById(form.getManagerId());
@@ -47,5 +47,17 @@ public class StoreService {
      */
     public boolean isNameExist(String name) {
         return storeRepository.findByName(name).isPresent();
+    }
+
+    /**
+     * store 이름으로 store 객체 찾기
+     *
+     * @param name
+     * @return Store
+     */
+    public Optional<Store> findStore(String name) {
+        Optional<Store> store = storeRepository.findByName(name);
+
+        return store;
     }
 }
